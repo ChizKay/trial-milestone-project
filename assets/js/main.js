b7d904b224fb0fbbfc59e99a42f759e720e70b0b
Ubuntu <ubuntu@ip-172-31-83-132.ec2.internal>
2019-11-05 18:55:37 +0000

    intial commit

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+var countyData = [{
+        name: "Kilkenny",
+        about: "Kilkenny is a medieval town in southeast Ireland. Its grand Kilkenny Castle was built in 1195 by Norman occupiers. The town has deep religious roots and many well-preserved churches and monasteries, including imposing St. Canice’s Cathedraland the Black Abbey Dominican priory, both from the 13th century. It's also a crafts hub, with shops along its winding lanes selling pottery, paintings and jewellery.",
+
+    },
+    {
+        name: "Galway",
+        about: "Galway, a harbour city on Ireland’s west coast, sits where the River Corrib meets the Atlantic Ocean. The city’s hub is 18th-century Eyre Square, a popular meeting spot surrounded by shops and traditional pubs that often offer live Irish folk music. Nearby, stone-clad cafes, boutiques and art galleries line the winding lanes of the Latin Quarter, which retains portions of the medieval city walls."
+    },
+    {
+        name: "Cork",
+        about: "Cork is a city in south-west Ireland, in the province of Munster. As of the 2016 census, the city had a population of 125,657, but following a boundary extension in 2019, the population increased to c. 210,000. It is the second largest city in the Republic of Ireland"
+
+    },
+    {
+        name: "Donegal",
+        about: "County Donegal is a largely Irish-speaking region bordering the Atlantic Ocean in northwestern Ireland. It’s made up of castles, rugged coastline and mountains such as the quartzite Mount Errigal. Glenveagh National Park,once a private estate, encompasses forests, lakes and bogland in the Derryveagh Mountains. Its 1870s manor house, the Scottish Baronial-style Glenveagh Castle, is known for its Victorian gardens."
+
+    },
+    {
+        name: "Dublin",
+        about: "Dublin, capital of the Republic of Ireland, is on Ireland’s east coast at the mouth of the River Liffey. Its historic buildings include Dublin Castle, dating to the 13th century, and imposing St Patrick’s Cathedral, founded in 1191. City parks include landscaped St Stephen’s Green and huge Phoenix Park, containing Dublin Zoo. The National Museum of Ireland explores Irish heritage and culture."
+
+    }
+
+];
+
+document.getElementById("county").innerHTML = `
+<h1 class="county-title">Places To Visit In Ireland.</h1>
+${countyData.map(function(counties){
+    return `<div class="places">
+    <h2>${counties.name}</h2>
+    <p>${counties.about}
+    </div>`
+}).join("")}
+`
+
+
+
+
+
+
---
+++ assets/js/maps.js
@@ -0,0 +1,108 @@
+function initMap() {
+    // Styles Map in night mode.
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: 53.1424,
+            lng: -7.6921
+        },
+        zoom: 4,
+        styles: [
+            { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+            { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{ color: '#263c3f' }]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#6b9a76' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{ color: '#38414e' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#212a37' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#9ca5b3' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{ color: '#746855' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{ color: '#1f2835' }]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#f3d19c' }]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{ color: '#2f3948' }]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#d59563' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{ color: '#17263c' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#515c6d' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{ color: '#17263c' }]
+            }
+        ]
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 52.6541, lng: -7.2448 },
+        { lat: 53.2707, lng: -9.0568 },
+        { lat: 51.8985, lng: -8.4756 },
+        { lat: 54.6538, lng: -8.1096 },
+        { lat: 53.3498, lng: -6.2603 },
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
